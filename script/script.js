95681db0788f3eff704efbc3cd8702f5d7a95dac
VetalSh <jedvemya86@gmail.com>
2020-06-05 15:56:53 +0300

    lesson 1

--- script/script.js
+++ script/script.js
@@ -1,6 +1,20 @@
-let message = function() {
-  alert('Изучил git и github');
-  console.log('Изучил git и github');
+let money = 1000,
+    income = 2000,    
+    addExpenses = 100,    
+    deposit = 900,    
+    mission = 'Create trading cites with comfortable funtional',    
+    period = '1 week';
+
+alert('Чтобы просмотреть выполненое задание нажмите клавишу F12 и кнопку "Смотреть выполненое ДЗ"');
+
+let message = function() {  
+  console.log('Создаем переменные: ');
+  console.log('money: ', money);
+  console.log('income: ', income);
+  console.log('addExpenses: ', addExpenses);
+  console.log('deposit: ', deposit);
+  console.log('mission: ', mission);
+  console.log('period: ', period);
 };
 
 document.getElementById('github').addEventListener('click', message);
