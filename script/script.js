b536ecba6bc5c779567038ce032f2977f10e5764
VetalSh <jedvemya86@gmail.com>
2020-06-08 19:53:45 +0300

    Homework 2

--- script/script.js
+++ script/script.js
@@ -1,20 +1,29 @@
 let money = 1000,
     income = 2000,    
-    addExpenses = 100,    
-    deposit = 900,    
-    mission = 'Create trading cites with comfortable funtional',    
-    period = '1 week';
+    addExpenses = 'Интернет, питание, коммунальные услуги, проезд',
+    deposit = true,    
+    mission = 9000,    
+    period = 3,
+    budgetDay;
 
-alert('Чтобы просмотреть выполненое задание нажмите клавишу F12 и кнопку "Смотреть выполненое ДЗ"');
 
 let message = function() {  
-  console.log('Создаем переменные: ');
-  console.log('money: ', money);
-  console.log('income: ', income);
-  console.log('addExpenses: ', addExpenses);
-  console.log('deposit: ', deposit);
-  console.log('mission: ', mission);
-  console.log('period: ', period);
+  console.log('Выводим типы данных для переменных: ');
+  console.log('money: ', typeof money);
+  console.log('income: ', typeof income);
+  console.log('deposit: ', typeof deposit);
+  
+  console.log('Длина стороки addExpenses: ', addExpenses.length);
+
+  console.log('Период равен ' + period + ' месяцев');
+  console.log('Цель заработать ' + mission + ' долларов');
+
+  console.log('Строка addExpenses приведена к нижнему регистру: ', addExpenses.toLowerCase());
+  let arrExpenses = addExpenses.toLowerCase().split(', ');
+  console.log('Разбиваем строку addExpenses на массив: ', arrExpenses);
+
+  budgetDay = (money + income) / 30;
+  console.log('budgetDay: ', budgetDay);
 };
 
 document.getElementById('github').addEventListener('click', message);
