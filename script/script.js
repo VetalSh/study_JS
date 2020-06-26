c16a862c53bb9465e48b8a810ced2b9129961624
VetalSh <jedvemya86@gmail.com>
2020-06-26 16:22:32 +0300

    Lesson 14 full version

--- script/script.js
+++ script/script.js
@@ -1,5 +1,77 @@
 'use strict';
 
+// let car = {
+//   doors: 4,
+//   turbocharging: false,
+//   ride: function() {
+//     console.log('Car ride');
+//   }
+// };
+
+// let newCar = Object.create(car);
+
+// newCar.model = 'mazda 3';
+
+// console.log(newCar.doors);
+// console.log(newCar.hasOwnProperty('model'));
+// console.log(newCar.__proto__.hasOwnProperty('doors'));
+// console.log(car.isPrototypeOf(newCar));
+
+// function Car(brand, model, options) {
+//   this.brand = brand;
+//   this.model = model;
+//   options = options || {};
+//   this.color = options.color;
+//   this.transmission = options.transmission;
+// }
+
+// Car.prototype.ride = function() {
+//   console.log(this.brand + ' ' + this.model + ' is ride!');
+// }
+
+// let car1 = new Car('Mazda', '3', {color: 'blue'});
+// console.log('car1: ', car1);
+// let car2 = new Car('VAZ', 'Niva', {ABS: false});
+// console.log('car2: ', car2);
+
+// car1.ride();
+// car2.ride();
+
+// console.log(Car.prototype.isPrototypeOf(car1));
+// console.log(car2 instanceof Car);
+
+// console.log(car1.ride === car2.ride);
+// console.log('car1: ', car1);
+
+// function Car(countryBuild, options) {
+//   this.countryBuild = countryBuild;
+//   options = options || {};
+//   this.color = options.color;
+//   this.transmission = options.transmission;
+// }
+
+// Car.prototype.ride = function() {
+//   console.log(this.brand + ' ' + this.model + ' is ride!');
+// }
+
+// function Audi(countryBuild, options, model, type) {
+//   this.brand = 'Audi';
+//   Car.apply(this, arguments);
+//   this.model = model;
+//   this.type = type;
+// }
+
+// Audi.prototype = Object.create(Car.prototype);
+// Audi.prototype.constructor = Audi;
+
+// let car_q7 = new Audi('germany', {color: 'red'}, 'Q7', 'S');
+// console.log('car_q7: ', car_q7);
+// console.log(car_q7 instanceof Audi);
+// console.log(car_q7 instanceof Car);
+// car_q7.ride();
+// console.log(new Object());
+// console.log(car_q7 instanceof Object);
+
 function DomElement(selector, height, width, bg, fontSize) {
   this.selector = selector;
   this.height = height;
@@ -32,7 +104,12 @@ DomElement.prototype.createElem = function() {
     background-color: ${this.bg};
     font-size: ${this.fontSize}px;
   `;
-
+  // Другой способ задания стилей
+  // newElem.style.height = this.height + 'px';
+  // newElem.style.width = this.width + 'px';
+  // newElem.style.backgroundColor = this.bg;
+  // newElem.style.fontSize = this.fontSize + 'px';
+  
   document.body.append(newElem);  
 };
 
