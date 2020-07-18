cb80bf7b28f2b5ecb8c0a8df43dba3f31c17c35c
VetalSh <jedvemya86@gmail.com>
2020-07-18 14:21:07 +0300

    Lesson 28 practice1

--- script/script.js
+++ script/script.js
@@ -1,65 +1,50 @@
 'use strict';
 
-let date = new Date(),
-    weekDay = date.getDay(),
-    hoursNow = date.getHours(),
-    newElem = document.createElement('div'),
-    newElem2 = document.createElement('div'),
-    newElem3 = document.createElement('div');
+const output = document.getElementById('output');
 
-if (hoursNow > 3 && hoursNow <= 9) {
-  newElem.innerHTML = 'Доброе утро';
-  document.body.append(newElem);
-} else if (hoursNow > 9 && hoursNow <= 15) {
-  newElem.innerHTML = 'Добрый день';
-  document.body.append(newElem);
-} else if (hoursNow > 15 && hoursNow <= 21) {
-  newElem.innerHTML = 'Добрый вечер';
-  document.body.append(newElem);
-} else if ((hoursNow > 21 && hoursNow <= 24) || (hoursNow >= 0 && hoursNow <= 3)) {
-  newElem.innerHTML = 'Доброй ночи';
-  document.body.append(newElem);
-}
+const getData = (url) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState !== 4) {
+        return;
+      }
+      if (request.status === 200) {
+        const response = JSON.parse(request.responseText);
+        resolve(response);
+      } else {
+        reject(request.statusText);
+      }
+    });
+    request.send();
+  });  
+};
 
-switch (weekDay) {
-  case 0: newElem2.innerHTML = 'Сегодня: Воскресенье';
-          document.body.append(newElem2);
-          break;
-  case 1: newElem2.innerHTML = 'Сегодня: Понедельник';
-          document.body.append(newElem2);
-          break;
-  case 2: newElem2.innerHTML = 'Сегодня: Вторник';
-          document.body.append(newElem2);
-          break;
-  case 3: newElem2.innerHTML = 'Сегодня: Среда';
-          document.body.append(newElem2);
-          break;
-  case 4: newElem2.innerHTML = 'Сегодня: Четверг';
-          document.body.append(newElem2);
-          break;
-  case 5: newElem2.innerHTML = 'Сегодня: Пятница';
-          document.body.append(newElem2);
-          break;
-  case 6: newElem2.innerHTML = 'Сегодня: Суббота';
-          document.body.append(newElem2);
-          break;
-  default: newElem2.innerHTML = 'Ошибка! День недели определить не удалось!';
-          document.body.append(newElem2);
-          break;
-}
+const outputPhotos = (data) => {
+  // выводим данные на страницу
+  data.forEach((item) => {
+    output.insertAdjacentHTML('beforebegin', 
+    `<h4>${item.title}</h4>
+    <img src="${item.thumbnailUrl}" alt="${item.title}"></img>`);
+  });
+  
+};
 
-newElem3.innerHTML = `Текущее время: ${date.toLocaleTimeString('en')}`;
-document.body.append(newElem3);
+const urlPhotos = 'https://jsonplaceholder.typicode.com/photos';
 
-function countTimer(deadline) {
-  let dateStop = new Date(deadline).getTime(),  
-      dateNow = date.getTime(),
-      timeRemaining = (dateStop - dateNow) / 1000,
-      days = Math.floor(timeRemaining / 3600 / 24),
-      newElem4 = document.createElement('div');
+const oneImg = getData('https://jsonplaceholder.typicode.com/photos/1'),
+      twoImg = getData('https://jsonplaceholder.typicode.com/photos/2'),
+      threeImg = getData('https://jsonplaceholder.typicode.com/photos/3');
 
-  newElem4.innerHTML = `До нового года осталось ${days} дней`;
-  document.body.append(newElem4);
-}
+// oneImg
+//   .then(outputPhotos)
+//   .catch(error => console.error(error));
 
-countTimer('01 january 2021');
+// twoImg
+//   .then(outputPhotos)
+//   .catch(error => console.error(error));
+
+Promise.all([threeImg, oneImg, twoImg])
+  .then(outputPhotos)
+  .catch(error => console.log(error));
